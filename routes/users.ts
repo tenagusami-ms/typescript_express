dfd87419616020c8bd3716a31de30b41927fbd75
KAN-YA, Yukitoshi <tenagusami@hotmail.com>
2021-02-28 17:12:47 +0900

    refactor: users script is changed to ts

--- routes/users.ts
+++ routes/users.ts
@@ -1,8 +1,10 @@
+import {Request, Response} from "express";
+
 const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.get('/', function(req: Request, res: Response) {
   res.send('respond with a resource');
 });
 
